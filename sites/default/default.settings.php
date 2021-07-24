f81d394015d1145393727ab61fac8e50aaccdf73
Zaid J. Barghouthi <barghouthi.z@gmail.com>
2021-07-24 23:17:24 +0300

    Initial commit

---
+++ sites/default/default.settings.php
@@ -0,0 +1,24 @@
+<?php
+
+$databases = [];
+
+$settings['hash_salt'] = '';
+
+$settings['update_free_access'] = FALSE;
+
+$settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
+
+$settings['file_scan_ignore_directories'] = [
+  'node_modules',
+  'bower_components',
+];
+
+$settings['entity_update_batch_size'] = 50;
+
+$settings['entity_update_backup'] = TRUE;
+
+$settings['migrate_node_migrate_type_classic'] = FALSE;
+
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
+}
